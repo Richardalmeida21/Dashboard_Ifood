30a256bc18f6dd0f1740167cb68f5df224f83835
Richardalmeida21 <richard.c.dealmeida@gmail.com>
2024-09-11 20:11:23 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sideMenu = document.querySelector('aside');
-    const menuBtn = document.querySelector('#menu_bar');
-    const closeBtn = document.querySelector('#close_btn');
-    const themeToggle = document.querySelector('.theme-toggler');
-    const sidebarLinks = document.querySelectorAll('.sidebar a');
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu_bar');
+const closeBtn = document.querySelector('#close_btn');
+const themeToggle = document.querySelector('.theme-toggler');
 
-    menuBtn.addEventListener('click', () => {
-        if (window.innerWidth <= 768) {
-            sideMenu.style.display = 'block';
-        }
-    });
+menuBtn.addEventListener('click', () => {
+    if (window.innerWidth <= 768) {
+        sideMenu.style.display = 'block';
+    }
+});
 
-    closeBtn.addEventListener('click', () => {
-        if (window.innerWidth <= 768) {
-            sideMenu.style.display = 'none';
-        }
-    });
+closeBtn.addEventListener('click', () => {
+    if (window.innerWidth <= 768) {
+        sideMenu.style.display = 'none';
+    }
+});
 
-    themeToggle.addEventListener('click', () => {
-        document.body.classList.toggle('dark-theme-variables');
-        themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
-        themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
-    });
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+    themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
+});
 
-    sidebarLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            sidebarLinks.forEach(link => link.classList.remove('active'));
-            link.classList.add('active');
-        });
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sidebarLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
     });
 });
